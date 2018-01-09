daed24f59711b999b9d2b1ab0893fef32c1d9a30
bvvy <bvvy@qq.com>
2018-01-09 23:31:11 +0800

    responser

--- src/main/java/com/divide2/base/service/impl/BaseServiceImpl.java
+++ src/main/java/com/divide2/base/service/impl/BaseServiceImpl.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.io.Serializable;
 import java.util.List;
 
--- src/main/java/com/divide2/constant/ReturnCoder.java
+++ src/main/java/com/divide2/constant/ReturnCoder.java
@@ -1,8 +1,5 @@
 package com.divide2.constant;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 /**
  * action_status
  * add_success
