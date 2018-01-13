b5644af9cf5477ea07293625c0ac9bab3b4bf3f0
yehang <yehang@sanduspace.cn>
2018-01-13 14:38:49 +0800

    no thing

--- src/main/java/com/divide2/search/SearchQuery.java
+++ src/main/java/com/divide2/search/SearchQuery.java
@@ -1,13 +1,9 @@
 package com.divide2.search;
 
-import java.awt.print.Pageable;
-
 /**
  * Created by bvvy on 2018/1/7.
  * com.divide2.search
  */
-public class SearchQuery {
-
-    private Pageable pageable;
+public interface SearchQuery {
 
 }
