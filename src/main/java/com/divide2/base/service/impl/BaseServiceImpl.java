ae1faad90de4c05f9007f74e5e8bd940626cb164
yehang <yehang@sanduspace.cn>
2018-01-16 19:58:44 +0800

    是不是可行了

--- src/main/java/com/divide2/base/service/impl/BaseServiceImpl.java
+++ src/main/java/com/divide2/base/service/impl/BaseServiceImpl.java
@@ -2,16 +2,25 @@ package com.divide2.base.service.impl;
 
 import com.divide2.base.service.BaseService;
 import com.divide2.search.Queryer;
+import com.divide2.search.annotation.Conditioner;
 import com.divide2.search.annotation.Deleter;
 import com.divide2.search.annotation.Inserter;
 import com.divide2.search.annotation.Updater;
+import org.apache.commons.lang.reflect.FieldUtils;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.Map;
 
@@ -19,10 +28,13 @@ import java.util.Map;
  * Created by bvvy on 2018/1/7.
  * com.divide2.base.service.impl
  */
-public abstract class BaseServiceImpl<T,ID extends Serializable,REPO extends JpaRepository<T,ID>> implements BaseService<T,ID> {
+public abstract class BaseServiceImpl<T, ID extends Serializable, REPO extends JpaRepository<T, ID>> implements BaseService<T, ID> {
 
     @Autowired
     private REPO repo;
+
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
 
     @Override
     @Inserter
@@ -52,6 +64,7 @@ public abstract class BaseServiceImpl<T,ID extends Serializable,REPO extends Jpa
     public List<T> all() {
         return repo.findAll();
     }
+
     //todo sort
     @Override
     public Page<T> page(Pageable pageable) {
@@ -61,13 +74,47 @@ public abstract class BaseServiceImpl<T,ID extends Serializable,REPO extends Jpa
     @Override
     //todo 自定义的 search 简化方法 看怎么来方便
     public Page<T> search(Queryer query) {
-
         return repo.findAll(new PageRequest(0, 2));
     }
 
+    @SuppressWarnings("unchecked")
+    private Class<T> getTClz() {
+        return (Class<T>) (((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+    }
+
+
     @Override
     public Page<T> search(Map<String, String> conditions) {
-        return repo.findAll(new PageRequest(0, 2));
+        Field[] fields = getTClz().getDeclaredFields();
+        BoolQueryBuilder queryBuilder = new BoolQueryBuilder();
+        for (Field field : fields) {
+            Conditioner conditioner = field.getAnnotation(Conditioner.class);
+            String fieldName = field.getName();
+            if (conditioner != null) {
+                switch (conditioner.way()) {
+                    case EQ: {
+                        queryBuilder.must(QueryBuilders.termQuery(fieldName, conditions.get(fieldName)));
+                    }
+                    case LIKE: {
+                        queryBuilder.must(QueryBuilders.queryStringQuery(fieldName).field(fieldName));
+                    }
+                    case RANGE: {
+                        queryBuilder.must(
+                                QueryBuilders.rangeQuery(fieldName)
+                                        .gte(
+                                                conditions.get(conditioner.startName())
+                                        )
+                                        .lte(conditions.get(conditioner.endName()))
+                        );
+                    }
+                }
+            }
+        }
+        SearchQuery searchQuery = new NativeSearchQuery(queryBuilder);
+        Integer page = Integer.parseInt(conditions.get("page"));
+        Integer size = Integer.parseInt(conditions.get("size"));
+        searchQuery.setPageable(new PageRequest(page, size));
+        return elasticsearchTemplate.queryForPage(searchQuery, getTClz());
     }
 
 }
